b9033c32b5f7180d77eb525b073ae808b1af154a
Caskey5 <siljacantonio@gmail.com>
2025-09-01 21:04:42 +0200

    vjezbe za varijable

---
+++ var_02.py
@@ -0,0 +1,21 @@
+"""
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću 
+vrijednost) za:
+ • Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, 
+težinu te spol
+ • Stranice a i b, četverokuta te za površinu tog četverokuta.
+
+"""
+
+first_name = "Antonio"
+last_name = "Siljac"
+year_of_birth = 1999
+country_of_birth = "Hrvatska"
+employment_status = True
+weight = 85.5
+gender = "male"
+
+a = 5
+b = 10
+area = a * b
+print("Površina četverokuta je:", area)
